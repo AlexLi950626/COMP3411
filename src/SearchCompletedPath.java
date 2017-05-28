b23a87f63de5c0c4d8f60286ebf76ac4510c7994
shiyun <shiyun.zhangsyz@gmail.com>
2017-05-28 13:00:49 +1000

    commenting

--- src/SearchCompletedPath.java
+++ src/SearchCompletedPath.java
@@ -87,7 +87,7 @@ public class SearchCompletedPath{
         while(!statePQ.isEmpty()){
             // get a state from queue
             SearchState currentState = statePQ.poll();
-            //currentState.printSearchState();
+            currentState.printSearchState();
             // if destination is reached return path
             Position currPosition = currentState.getAgentPosition();
             char currPositionType = currentState.getTypeInBoard(currPosition.getRow(), currPosition.getCol());
